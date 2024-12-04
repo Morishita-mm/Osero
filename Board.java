80bb31875d16cd0f22b679d67e8fbabd7d145f67
Morishita-mm <mizuki.no.onaka@gmail.com>
2024-12-04 10:29:08 +0900

    add: put cell feature

--- Board.java
+++ Board.java
@@ -1,19 +1,48 @@
 package OseroApp;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class Board {
-    private ArrayList<Dist> distList = new ArrayList<>();
+    static final String LINE_SEP = System.getProperty("line.separator");
+
+    private int[][] board = new int[8][8];
+    private String[][] visBoard = new String[8][8];
 
     public Board() {
-        this.distList.add(new Dist(true, 3, 3));
-        this.distList.add(new Dist(false, 3, 4));
-        this.distList.add(new Dist(false, 4, 3));
-        this.distList.add(new Dist(true, 4, 4));
+        this.addCell(3, 3, 1);
+        this.addCell(3, 4, 0);
+        this.addCell(4, 3, 0);
+        this.addCell(4, 4, 1);
+    }
+
+    public void addCell(int row, int col, int color) {
+        // 盤内かどうかの判定
+        if (col < 0 || col >= 8 || row < 0 || row >= 8) {
+            System.err.println("選択された位置が盤外です");
+            return;
+        }
+
+        // 駒が置かれていないかどうかの判定
+        if (visBoard[col][row] != null) {
+            System.err.println("すでに駒が置かれています");
+            return;
+        }
+
+        this.board[col][row] = color;
+        this.visBoard[col][row] = (color == 1) ? "⚪︎" : "⚫️";
     }
 
     public String toString() {
-        return "";
+        StringBuffer sb = new StringBuffer();
+        for (String[] vb : this.visBoard) {
+            sb.append("ー".repeat(12)).append(LINE_SEP);
+            String[] replacedVb = Arrays.stream(vb)
+                    .map(s -> s == null ? "　" : s)
+                    .toArray(String[]::new);
+            sb.append(String.join("|", replacedVb)).append(LINE_SEP);
+        }
+        return sb.toString();
     }
 }
--- main.java
+++ main.java
@@ -1,18 +1,26 @@
 package OseroApp;
 
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
         System.out.println("Game Start!");
         Board board = new Board();
-        boolean isBlack = true;
+        try (Scanner sc = new java.util.Scanner(System.in)) {
+            int color = 1;
+            int col;
+            int row;
 
-        for (int i = 0; i < 64; i++) {
-            if ((i % 2) == 0) {
-                isBlack = true;
-            } else {
-                isBlack = false;
+            while (true) {
+                color = (color == 1) ? 0 : 1;
+                System.out.print("Colum > ");
+                col = sc.nextInt();
+                System.out.print("Row > ");
+                row = sc.nextInt();
+
+                board.addCell(row, col, color);
+                System.out.println(board);
             }
         }
-        System.out.println(board);
     }
 }
